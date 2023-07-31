6c48cfc0c2b5e410cdbd97abaf2649037aa4fb60
Aditya <aditya.rai@weboccult.com>
2023-07-31 13:12:16 +0530

    Handled missing case

--- train.py
+++ train.py
@@ -11,8 +11,7 @@ MODEL_ENDPOINT = os.getenv("MODEL_ENDPOINT")
 VERSION = os.getenv("VERSION")
 EPOCHS = os.getenv("EPOCHS")
 
-project_name =f"{MODEL_ENDPOINT.split('-')[0]}-{VERSION}"            # Give your own project_name = your_project_name-version
-
+project_name =f"{MODEL_ENDPOINT.split('-')[0]}-{VERSION}"            
 
 # Setting the device
 
@@ -21,32 +20,31 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 
-# Here in the below example the name inside .project() is the project name, here dog_nose_detection, ignore the last five characters
-# The version is written in project.version(1), here 1 is the version
 
-# Example (this will be recieved from roboflow)
-# project = rf.workspace("dog-nose-detection").project("dog_nose_detection-uipkm")
-# dataset = project.version(1).download("yolov8")
 
-try:
 
-    if project_name == "":
-        print("Please enter Project Name")
-    else:
+if project_name == "":
+    print("Please enter Project Name")
+else:
+    try:
         if not os.path.exists(os.getcwd()+"/"+project_name):
 
             # Replace the below three lines with the code snippet obtained from roboflow
             rf = Roboflow(api_key=API_KEY)
             project = rf.workspace().project(MODEL_ENDPOINT)
             dataset = project.version(VERSION).download("yolov8")
-except:
-    print("Please verify the values entered in the .env file.")
 
-model = YOLO("yolov8n.pt")
-model.train(
-    data=os.getcwd()+f"/{project_name}/data.yaml",   # path of the data.yaml present in the downloaded dataset folder
-    epochs=int(EPOCHS),
-    device = device
-)
+        model = YOLO("yolov8n.pt")
+        model.train(
+            data=os.getcwd()+f"/{project_name}/data.yaml",   # path of the data.yaml present in the downloaded dataset folder
+            epochs=int(EPOCHS),
+            device = device
+    )
+    except KeyboardInterrupt:
+        print("Iterrupted")
+    except:
+        print("Please verify the values entered in the .env file.")
+
+    
 
 # the best.pt and last.pt will be saved at latest version of runs/detect/latest_version_of_train/weights
