190e7207068988e2908646a4c238642f0651a82c
Aditya <aditya.rai@weboccult.com>
2023-07-31 13:04:52 +0530

    Added .env file

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Custom Dog nose detection training.
 ## _Training custom yolov8 model_
 
-## Complete Documentation https://docs.google.com/document/d/1mXVtjtAF0Q6n7Fotgim636RbiLkEczvP0wkDmCztcvM/edit
-
 ## Steps
 
 - Create a account in roboflow : https://roboflow.com/
@@ -28,15 +26,11 @@ Clone the repository
 git clone https://github.com/adityaweboccult/custom_training_with_roboflow.git
 ```
 Change the directory
-```sh
-cd custom_training_with_roboflow
-```
 
 Install the packages
 ```sh
 pip install -r requirements.txt
 ```
-
 Install Pytorch https://pytorch.org/
 
 GPU
@@ -47,8 +41,7 @@ CPU
 ```sh
 conda install pytorch torchvision torchaudio cpuonly -c pytorch
 ```
-
-Change the project_name and the code snippet obtained from roboflow in the `train.py` file
+Change the API_KEY, MODEL_ENDPOINT, VERSION and EPOCHS in the .env file.
 
 Run the code
 
--- train.py
+++ train.py
@@ -2,6 +2,17 @@ from ultralytics import YOLO
 from roboflow import Roboflow
 import os
 import torch
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+MODEL_ENDPOINT = os.getenv("MODEL_ENDPOINT")
+VERSION = os.getenv("VERSION")
+EPOCHS = os.getenv("EPOCHS")
+
+project_name =f"{MODEL_ENDPOINT.split('-')[0]}-{VERSION}"            # Give your own project_name = your_project_name-version
+
 
 # Setting the device
 
@@ -10,10 +21,6 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 
-print(device)
-
-
-
 # Here in the below example the name inside .project() is the project name, here dog_nose_detection, ignore the last five characters
 # The version is written in project.version(1), here 1 is the version
 
@@ -21,25 +28,25 @@ print(device)
 # project = rf.workspace("dog-nose-detection").project("dog_nose_detection-uipkm")
 # dataset = project.version(1).download("yolov8")
 
+try:
 
-project_name = "dog_nose_detection-1"           # Give your own project_name = your_project_name-version
-EPOCH = 3                                       # Change the Number of epochs to train the model
+    if project_name == "":
+        print("Please enter Project Name")
+    else:
+        if not os.path.exists(os.getcwd()+"/"+project_name):
 
-if project_name == "":
-    print("Please enter Project Name")
-else:
-    if not os.path.exists(os.getcwd()+"/"+project_name):
+            # Replace the below three lines with the code snippet obtained from roboflow
+            rf = Roboflow(api_key=API_KEY)
+            project = rf.workspace().project(MODEL_ENDPOINT)
+            dataset = project.version(VERSION).download("yolov8")
+except:
+    print("Please verify the values entered in the .env file.")
 
-        # Replace the below three lines with the code snippet obtained from roboflow
-        rf = Roboflow(api_key="cr5ilWXHdJYSGrisqBwa")
-        project = rf.workspace("dog-nose-detection").project("dog_nose_detection-uipkm")
-        dataset = project.version(1).download("yolov8")
-
-    model = YOLO("yolov8n.pt")
-    model.train(
-        data=os.getcwd()+f"/{project_name}/data.yaml",   # path of the data.yaml present in the downloaded dataset folder
-        epochs=EPOCH,
-        device = device
-    )
+model = YOLO("yolov8n.pt")
+model.train(
+    data=os.getcwd()+f"/{project_name}/data.yaml",   # path of the data.yaml present in the downloaded dataset folder
+    epochs=int(EPOCHS),
+    device = device
+)
 
 # the best.pt and last.pt will be saved at latest version of runs/detect/latest_version_of_train/weights
