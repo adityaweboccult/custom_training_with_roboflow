cc5c92b9bfd8cc4bdc16bf564e84f2a9e29acf0d
Aditya <aditya.rai@weboccult.com>
2023-07-31 14:54:53 +0530

    dynamically taking project name

--- train.py
+++ train.py
@@ -11,8 +11,6 @@ MODEL_ENDPOINT = os.getenv("MODEL_ENDPOINT")
 VERSION = os.getenv("VERSION")
 EPOCHS = os.getenv("EPOCHS")
 
-project_name =f"{MODEL_ENDPOINT.split('-')[0]}-{VERSION}"            
-
 # Setting the device
 
 if torch.cuda.is_available():
@@ -21,30 +19,27 @@ else:
     device = "cpu"
 
 
+try:
 
-
-if project_name == "":
-    print("Please enter Project Name")
-else:
-    try:
-        if not os.path.exists(os.getcwd()+"/"+project_name):
-
-            # Replace the below three lines with the code snippet obtained from roboflow
-            rf = Roboflow(api_key=API_KEY)
-            project = rf.workspace().project(MODEL_ENDPOINT)
-            dataset = project.version(VERSION).download("yolov8")
-
-        model = YOLO("yolov8n.pt")
-        model.train(
-            data=os.getcwd()+f"/{project_name}/data.yaml",   # path of the data.yaml present in the downloaded dataset folder
-            epochs=int(EPOCHS),
-            device = device
-    )
-    except KeyboardInterrupt:
-        print("Iterrupted")
-    except:
-        print("Please verify the values entered in the .env file.")
-
+    rf = Roboflow(api_key=API_KEY)
+    project = rf.workspace().project(MODEL_ENDPOINT)
     
+    # Extracting and setting the project_name
+    project_name = project.name.replace(" ","-") + "-" + str(VERSION)
+    if not os.path.exists(os.getcwd()+"/"+project_name):
+        dataset = project.version(VERSION).download("yolov8")
+
+    model = YOLO("yolov8n.pt")
+    model.train(
+        data=os.getcwd()+f"/{project_name}/data.yaml",   # path of the data.yaml present in the downloaded dataset folder
+        epochs=int(EPOCHS),
+        device = device
+)
+except KeyboardInterrupt:
+    print("Iterrupted")
+except:
+    print("Please verify the values entered in the .env file.")
+
+
 
 # the best.pt and last.pt will be saved at latest version of runs/detect/latest_version_of_train/weights
